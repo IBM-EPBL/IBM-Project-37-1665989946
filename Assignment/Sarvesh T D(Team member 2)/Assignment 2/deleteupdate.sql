af1292d28904a25eedcd978edcb41b31e4f52ff1
Sarvesh T.D <sarvesh22032002@gmail.com>
2022-10-20 18:42:30 +0530

    changes
    Co-Authored-By: Udhayakumar T <60465112+dev-udhayakumar@users.noreply.github.com>
    Co-Authored-By: Shan2910 <87259772+Shan2910@users.noreply.github.com>
    Co-Authored-By: vaishu1427 <72733097+vaishu1427@users.noreply.github.com>

---
+++ Assignment/Sarvesh T D(Team member 2)/Assignment 2/deleteupdate.sql
@@ -0,0 +1,17 @@
+create database user_details
+use user_details 
+create table team
+(
+userID int primary key,
+username varchar(50),
+password varchar(50),
+email varchar(50),
+rollno int
+)
+insert into details values (1,'Udhayakumar','udhayasiva9524','udhayasiva9524@gmail.com',711319CS170);
+insert into details values (2,'Sarvesh','sarvesh22032002','sarvesh22032002@gmail.com',711319CS142);
+insert into details values (3,'Shanmugasundaram','shanmugasundaram2910','shanmugasundaram2910@gmail.com',711319CS147);
+insert into details values (4,'Vaishnavi','Vaishnavi','Vaishnavi@gmail.com',711319CS171);
+select*from team
+delete from team where userID=4
+update team set username='Saran' where userID=4
